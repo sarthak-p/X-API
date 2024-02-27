d7758cc50c02223106d570637fd17fffd887fc11
ianrose <ianrose@umich.edu>
2024-02-27 15:33:41 -0600

    added Validate infrastructure

---
+++ twitter/src/main/java/com/cooksys/twitter/controllers/ValidateController.java
@@ -0,0 +1,14 @@
+package com.cooksys.twitter.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/validate")
+@AllArgsConstructor
+public class ValidateController {
+    // check whether a given hashtag exits
+
+    // check whether a given username exists
+
+    // check whether a given username is available
+}
---
+++ twitter/src/main/java/com/cooksys/twitter/services/impl/ValidateServiceImpl.java
@@ -0,0 +1,9 @@
+package com.cooksys.twitter.services.impl;
+
+import com.cooksys.twitter.services.ValidateService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ValidateServiceImpl implements ValidateService {
+
+}
