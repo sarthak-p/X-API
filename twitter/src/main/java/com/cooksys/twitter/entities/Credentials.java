849cb6ecdb56f26ab938dd8b092d0969e8ef046e
Rickey Weems <weems505@gmail.com>
2024-02-27 16:04:39 -0500

    Embeddable Update

--- twitter/src/main/java/com/cooksys/twitter/entities/Credentials.java
+++ twitter/src/main/java/com/cooksys/twitter/entities/Credentials.java
@@ -2,9 +2,11 @@ package com.cooksys.twitter.entities;
 
 import jakarta.persistence.Embeddable;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Data
+@NoArgsConstructor
 public class Credentials {
     private String username;
     private String password;
--- twitter/src/main/java/com/cooksys/twitter/entities/Profile.java
+++ twitter/src/main/java/com/cooksys/twitter/entities/Profile.java
@@ -2,9 +2,11 @@ package com.cooksys.twitter.entities;
 
 import jakarta.persistence.Embeddable;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Data
+@NoArgsConstructor
 public class Profile {
     private String firstName;
     private String lastName;
