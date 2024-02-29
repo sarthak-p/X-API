2ceb20e9310fc7c537c5d9db111aabfec6159f0d
ianrose <ianrose@umich.edu>
2024-02-29 11:56:11 -0600

    updated commit with controller

--- twitter/src/main/java/com/cooksys/twitter/controllers/TweetController.java
+++ twitter/src/main/java/com/cooksys/twitter/controllers/TweetController.java
@@ -2,6 +2,7 @@ package com.cooksys.twitter.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,9 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cooksys.twitter.dtos.CredentialsDto;
 import com.cooksys.twitter.dtos.TweetRequestDto;
 import com.cooksys.twitter.dtos.TweetResponseDto;
-import com.cooksys.twitter.entities.Tweet;
 import com.cooksys.twitter.services.TweetService;
 
 import lombok.RequiredArgsConstructor;
@@ -35,5 +36,10 @@ public class TweetController {
     @PostMapping
     public TweetResponseDto createTweet(@RequestBody TweetRequestDto tweetRequestDto) {
         return tweetService.createTweet(tweetRequestDto);
+    }
+    
+    @DeleteMapping("/{id}")
+    public TweetResponseDto deleteTweet(@RequestBody CredentialsDto credentialsDto, @PathVariable Long id) {
+    	return tweetService.deleteTweet(credentialsDto, id);
     }
 }
